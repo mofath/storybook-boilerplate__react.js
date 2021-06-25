8226fbd4db4a0ae49a3dfb499f0c5bfa186e6efd
mofath <mohamed.elgaedi@gmail.com>
2021-06-25 23:45:26 +0200

    Installed storybook and added main configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,20 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-action",
     "@storybook/links",
     "@storybook/addon-docs",
+    "@storybook/addon-contexts/register",
+    "@storybook/addon-backgrounds/register",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true
+      }
+    }
   ]
 }
