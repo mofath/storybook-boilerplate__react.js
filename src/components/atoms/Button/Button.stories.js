9465b1e21f48508cb213fa6b9cb2d4b6562c4ce4
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-12-22 01:38:18 +0200

    Add Icon button story

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, select, text } from '@storybook/addon-knobs';
 import { Button } from './Button';
+import knobData from './button.knobs.json';
 
-export const text = () => {
-    return <Button onClick={action('clicked')}>Click me</Button>;
+const { icon, innerText } = knobData;
+
+const buttonClicked = (e) => {
+  e.preventDefault();
+  alert('Hello');
 };
 
-export const emoji = () => {
-    return (
-        <Button onClick={action('clicked')}>
-            <span>😅 😂 🤣</span>
-        </Button>
-    );
-};
+export const basicButton = () => (
+  <Button onClick={action('clicked')}>
+    {text(innerText.label, innerText.default, innerText.group)}
+  </Button>
+);
+
+export const secondaryButton = () => (
+  <Button variant='secondary'>
+    {text(innerText.label, 'Secondary button', innerText.group)}
+  </Button>
+);
+
+export const tertiaryButton = () => (
+  <Button variant='tertiary'>
+    {text(innerText.label, 'Tertiary button', innerText.group)}
+  </Button>
+);
+
+export const iconButton = () => (
+  <Button icon='user'>
+    {text(innerText.label, 'Icon button', innerText.group)}
+  </Button>
+);
+
+export const functionButton = () => (
+  <Button onClick={buttonClicked}>
+    {text(innerText.label, 'Function button', innerText.group)}
+  </Button>
+);
+
+export const linkedButton = () => (
+  <Button href='/route'>
+    {text(innerText.label, 'Link button', innerText.group)}
+  </Button>
+);
 
 /* eslint import/no-anonymous-default-export: */
 export default {
-    component: Button,
-    title: 'Button',
+  component: Button,
+  decorators: [withKnobs],
+  title: 'Button',
 };
